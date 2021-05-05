905fed1d00aa0f56e45a22cee072b878f40e5f72
Arthur Kalule <kalulearthur@gmail.com>
2021-05-05 14:41:45 +0300

    add goal functionality for team A

--- app/src/main/java/comexample/android/americanfootballscore/MainActivity.java
+++ app/src/main/java/comexample/android/americanfootballscore/MainActivity.java
@@ -1,14 +1,61 @@
 package comexample.android.americanfootballscore;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    int teamAScore = 0;
+    int teamBScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method displays scores for team A
+     */
+    public void displayTeamAScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method adds 6 points to Team A when a touchdown is made
+     */
+    public void addSixPointsToTeamA(View view) {
+        teamAScore += 6;
+        displayTeamAScore(teamAScore);
+    }
+
+    /**
+     * This method adds 3 points to Team A when a field goal is made
+     */
+    public void addThreePointsToTeamA(View view) {
+        teamAScore += 3;
+        displayTeamAScore(teamAScore);
+    }
+
+    /**
+     * This method adds 2 points to Team A for safety  when Team B makes a tackle on them
+     * with the ball in their with the bold in their own end zone
+     */
+    public void addTwoPointsToTeamA(View view) {
+        teamAScore += 1;
+        displayTeamAScore(teamAScore);
+    }
+
+    /**
+     * This method adds 1 point to Team A when an extra point is earned
+     * when team A kicks through the uprights after a touchdown
+     */
+    public void addOnePointsToTeamA(View view) {
+        teamAScore += 1;
+        displayTeamAScore(teamAScore);
+    }
+
 }
