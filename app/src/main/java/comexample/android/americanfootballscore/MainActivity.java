d0088bfcb7d176a64665950a12c1916346e9251d
Arthur Kalule <kalulearthur@gmail.com>
2021-05-05 14:48:34 +0300

    add goal functionality for team B

--- app/src/main/java/comexample/android/americanfootballscore/MainActivity.java
+++ app/src/main/java/comexample/android/americanfootballscore/MainActivity.java
@@ -58,4 +58,46 @@ public class MainActivity extends AppCompatActivity {
         displayTeamAScore(teamAScore);
     }
 
+    /**
+     * This method displays scores for team A
+     */
+    public void displayTeamBScore(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method adds 6 points to Team A when a touchdown is made
+     */
+    public void addSixPointsToTeamB(View view) {
+        teamBScore += 6;
+        displayTeamAScore(teamBScore);
+    }
+
+    /**
+     * This method adds 3 points to Team B when a field goal is made
+     */
+    public void addThreePointsToTeamB(View view) {
+        teamBScore += 3;
+        displayTeamAScore(teamBScore);
+    }
+
+    /**
+     * This method adds 2 points to Team B for safety  when Team B makes a tackle on them
+     * with the ball in their with the bold in their own end zone
+     */
+    public void addTwoPointsToTeamB(View view) {
+        teamBScore += 1;
+        displayTeamBScore(teamBScore);
+    }
+
+    /**
+     * This method adds 1 point to Team B when an extra point is earned
+     * when team A kicks through the uprights after a touchdown
+     */
+    public void addOnePointsToTeamB(View view) {
+        teamBScore += 1;
+        displayTeamBScore(teamBScore);
+    }
+
 }
